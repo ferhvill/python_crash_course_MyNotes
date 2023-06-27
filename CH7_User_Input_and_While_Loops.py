b04462571fed68fe39d5bbf815a56fec1556a098
Fernando <110078268+ferhvill@users.noreply.github.com>
2023-06-27 19:38:15 -0400

    while examples added

--- CH7_User_Input_and_While_Loops.py
+++ CH7_User_Input_and_While_Loops.py
@@ -44,3 +44,22 @@ number = 1
 while number <= 5:
     print(number)
     number += 1
+# Example
+prompt = "\nTell me something, and I will repeat it back to you: "
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != "quit":
+    message = input(prompt)
+    if message != "quit":
+        print(message)
+
+# Example with a flag
+prompt = "\nTell me something, and I will repeat it back to you: "
+prompt += "\nEnter 'quit' to end the program. "
+active = True
+while active:
+    message = input(prompt)
+    if message == "quit":
+        active = False
+    else:
+        print(message)
